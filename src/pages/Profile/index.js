aaf1b934a463f7793cc9f83b4bbf91c2c579915c
Matheus Leite <matheusjoselfm@gmail.com>
2020-04-28 21:54:59 -0300

    Adicionando campos na tela de meu perfil e valores inicias

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector((state) => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input type="email" name="email" placeholder="Seu endereço de e-mail" />
+        <hr />
+
+        <Input name="oldPassword" placeholder="Sua senha atual" />
+        <Input name="password" placeholder="Nova senha" />
+        <Input name="confirmPassword" placeholder="Confirmação de senha" />
+
+        <button type="submit">Atualizar Perfil</button>
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
