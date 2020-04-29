47efd31e19c4e79e0c5ca421d4604e384080a54c
Matheus Leite <matheusjoselfm@gmail.com>
2020-04-29 00:43:37 -0300

    estilizando componentes criados na pagina do dashboard

--- src/pages/Dashborad/index.js
+++ src/pages/Dashborad/index.js
@@ -1,7 +1,42 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
+import api from '~/services/api';
+
+import { Container, Time } from './styles';
 
 export default function Dashborad() {
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>31 de Maio</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Matheus Leite</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Matheus Leite</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Matheus Leite</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
