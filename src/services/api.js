e8742b29a1acc6625f83ca0bb875e7f8c3909af2
Matheus Leite <matheusjoselfm@gmail.com>
2020-05-16 18:21:22 -0300

    adicionando url da api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://161.35.105.205/barber/api/v1/',
+  baseURL: 'https://matheusleite.site/barber/api/v1',
 });
 
 export default api;
